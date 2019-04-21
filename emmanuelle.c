108c5adca871a4bbe1c1300bde4902c818995b4a
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2019-04-20 21:31:52 -0500

    Finished prtTraversal() and added documentation to functions

--- emmanuelle.c
+++ emmanuelle.c
@@ -13,6 +13,24 @@ int main(int argc, char const *argv[])
 // void prtAll(Graph graph); 
 // void prtOne(Graph graph, int iVertex);
 
+/****************************** prtOne ******************************
+void prtOne(Graph graph, int iVertex);
+Purpose:
+ Prints information for airport at specified vertex.
+ 
+Parameters:
+    I Graph graph          		Graph containing connections of airports/flights.
+    I int iVertex        		Specified vertex that the function will print information
+    							from.
+
+Notes:
+    Prints information including Vertex#, Airport, Predecessors and Successors.
+    A maximum of 8 predecessors is assumed. If there are less than 8, empty spots
+    are replaced with "....."
+Returns 
+    N/A
+*******************************************************************************/
+
 void prtOne(Graph graph, int iVertex)
 {
 
@@ -63,9 +81,25 @@ void prtOne(Graph graph, int iVertex)
 
 }
 
+
+/****************************** prtAll ******************************
+void prtAll(Graph graph);
+Purpose:
+ Prints all vertices in a list/graph.
+ 
+Parameters:
+    I Graph graph          		Graph containing connections of airports/flights.
+
+Notes:
+    Prints all vertices in a list. Utilizes prtOne() to accomplish this.
+Returns 
+    N/A
+*******************************************************************************/
+
+
 void prtAll(Graph graph)
 {
-	printf("All Vertices In a List");
+	printf("All Vertices In a List\n");
 	int iVertex;							//vertex # initialized for loop
 	
 	//for each vertex in a list, call printall
@@ -76,55 +110,59 @@ void prtAll(Graph graph)
 }
 
 
-void printWithIndent(int indent, char * string);
-
-void printWithIndent(int indent, char * string)
-{
-    printf("%*s%s", indent, "", string);
-}
-
-//recursive functions
-// void prtTraversal(Graph graph, int iAirportVertex, int indent);
-
-//prtsucc szOrigin flight->szOrigin ? 
-
-// DFT
-// int countE(Graph g, int v, int visitedM[])
-// {
-//     int iCount= 0;
-// 	   EdgeNode *e;
-
-//    if(visitedM[v])
-// 	     return 0;
-//    visitedM[v] = TRUE;
-
-// for(e=g->vertexM[v].successorList; e != NULL; e=e->pNextEdge)
-// {
-//     iCount += + countE(g,e->iVertex, visitedM);
-// }
-//     }
+/****************************** prtTraversal ******************************
+void prtTraversal(Graph graph, int iAirportVertex, int indent);
+Purpose:
+ Prints flights that begin at the specified origin.
+ 
+Parameters:
+    I Graph graph          		Graph containing connections of airports/flights.
+    I int iAirportVertex        Specified vertex that the function will begin printing
+    							from.
+    I int indent          		int representing indentions in printed output.
+Notes:
+    prtTraversal is a recursive function. It will continue to make calls to itself
+    until the criteria for the base case is met (if aiport at specified vertex has
+    already been visited).
+Returns 
+    N/A
+*******************************************************************************/
 
 
 void prtTraversal(Graph graph, int iAirportVertex, int indent)
 {
 	
 	EdgeNode *e;
+	int i;
 
-	//base case
+	//base case —— see if airport has been visited
 	if(graph->vertexM[iAirportVertex].bVisited == TRUE)
 		return;
 
 	//change bVisited to TRUE
 	graph->vertexM[iAirportVertex].bVisited = TRUE;
 	char * successorAirport = "";
-	// printWithIndent(indent, string);
-	//idk how to use the indent lol
 
 
 	for(e = graph->vertexM[iAirportVertex].successorList; e != NULL; e=e->pNextEdge)
 	{
+
+		//calculate vertex for recursive call
 		int iVertexRecursive = findAirport(graph, graph->vertexM[iAirportVertex].szAirport);
-		//unsure about this one
+
+		//calculate arrivalTime using calcArr2400();
+		int iArrivalTime = calcArr2400(e->flight.iDepTm2400, e->flight.iDurationMins, e->flight.iZoneChange);
+		
+		//for-loop for indent
+		for(i = 0; i < indent; i++)
+		{
+			printf(" "); 	//print a space for from 0 to value specified at indent
+		}	
+
+		//print values
+		//F#-Apt DeptTime-ArrivalTime
+		printf("%s-%s %d-%d", e->flight.szFlightNr, graph->vertexM[iAirportVertex].szAirport, e->flight.iDepTm2400, iArrivalTime);
+		printf("\n");		//newline
 
 		prtTraversal(graph, iVertexRecursive, indent + 1);
 	}
@@ -145,6 +183,25 @@ int findAirport(Graph graph, char szAirport[])
 	return -1;
 }
 
-//program 6
-//deleteFlight
+int calcArr2400(int iDepTime2400, int iDurationMins, int iZoneChange)
+{  
+    int iHours;     // holds number of hours of iDepTime2400
+    int iMin;       // holds number of minutes of iDepTime2400
+    int iZoneAdd;   // holds number of minutes of time zone changes
+    int iTotalMin;  // holds total number of minutes of flight
+    int iArr2400;      // holds conversions of iTotalMin to 2400 time style
+    
+    // obtain number of hours
+    iHours = iDepTime2400 / 100;
+    // obtain number of minutes
+    iMin = iDepTime2400 % 100;
+    // obtain number of minutes for time zone changes
+    iZoneAdd = iZoneChange * 60;
+    // find total number of minutes of flight
+    iTotalMin = (iHours * 60) + iMin + iZoneAdd + iDurationMins;
+    // convert total number of minutes to 2400 time 
+    iArr2400 = (((iTotalMin / 60) * 100) + (iTotalMin % 60));
+    // return the new 2400 arrival time
+    return iArr2400;
+}
 
